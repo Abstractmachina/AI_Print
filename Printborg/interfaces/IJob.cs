f96215f2428e6e3fbd98543a913b63b1a5044757
Tao <taole.chen@gmail.com>
2024-01-12 23:14:56 +0100

    clean up

---
+++ Printborg/interfaces/IJob.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Printborg.Types.Deforum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printborg.Interfaces {
+    public interface IJob {
+        string Id { get; set; }
+        Status Status { get; set; }
+        string Phase { get; set; }
+        string ErrorType { get; set; }
+        double Progress { get; set; }
+        double StartedAt { get; set; }
+        double LastUpdated { get; set; }
+        double ExecutionTime { get; set; }
+        double UpdateIntervalTime { get; set; }
+        int Updates { get; set; }
+        string Message { get; set; }
+        string Outdir { get; set; }
+    }
+}
