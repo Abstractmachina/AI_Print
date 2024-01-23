721b4d1f3c0c17f4f9db567d6839dbc2318f1b90
Tao <taole.chen@gmail.com>
2024-01-23 13:16:14 +0100

    added DeforumSettings class and sub-types

---
+++ Printborg/types/DeforumSettings.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printborg.Types {
+    public class DeforumSettings {
+
+        public RunSettings Run { get; set; }
+        public KeyFramesSettings KeyFrames { get; set; }
+        public GuidedImagesSettings GuidedImages { get; set; }
+        public PromptSettings Prompt { get; set; }
+        public StrengthSettings Strength { get; set; }
+        public CfgSettings Cfg { get; set; }
+        public SeedSettings Seed { get; set; }
+        public MotionSettings Motion { get; set; }
+
+        public DeforumSettings() { }
+        public DeforumSettings(RunSettings run, KeyFramesSettings keyFrames, GuidedImagesSettings guidedImages, PromptSettings prompt, StrengthSettings strength, CfgSettings cfg, SeedSettings seed, MotionSettings motion) {
+            Run = run;
+            KeyFrames = keyFrames;
+            GuidedImages = guidedImages;
+            Prompt = prompt;
+            Strength = strength;
+            Cfg = cfg;
+            Seed = seed;
+            Motion = motion;
+        }
+
+    }
+
+    public struct RunSettings {
+        public string Sampler;
+        public int Steps;
+        public int W;
+        public int H;
+        public int Seed;
+        public string BatchName;
+    }
+
+    public struct KeyFramesSettings {
+        public string AnimationMode;
+        public int DiffusionCadence;
+        public int MaxFrames;
+        public int Fps;
+
+    }
+
+    public struct GuidedImagesSettings {
+        /// <summary>
+        /// syntax: first frame - "0": "path", 
+        /// then all following frames - 
+        /// "max_f{int-int}" : "path" or 
+        /// "max_f{int}" : "path"
+        /// </summary>
+        public Dictionary<string, string> InitImages;
+        // TODO find out correct structure
+        public string ImageStrengthSchedule;
+        public string BlendFactorMax;
+        public string BlendFactorSlope;
+        public string TweeningFramesSchedule;
+
+    }
+
+    public struct PromptSettings {
+        public Dictionary<int, string> Prompts;
+        public Dictionary<int, string> PositivePrompts;
+        public Dictionary<int, string> NegativePrompt;
+    }
+
+    public struct StrengthSettings {
+        public List<string> StrengthSchedule;
+    }
+
+    public struct  CfgSettings
+    {
+        public List<string> CfgScaleSchedule;
+    }
+
+    public struct SeedSettings {
+        public string SeedBehaviour;
+        public string SeedSchedule;
+    }
+
+    public struct MotionSettings {
+        public string Angle;      
+        public string Zoom;
+        public string TranslationX;
+        public string TranslationY;
+        public string TranslationZ;
+        public string TransformCenterX;
+        public string TransformCenterY;
+        public string Rotation3dX;
+        public string Rotation3dY;
+        public string Rotation3dZ;
+
+    }
+
+}
---
+++ PrintborgGH/GH_Types/GH_DeforumSettings.cs
@@ -0,0 +1,53 @@
+﻿using Grasshopper.Kernel.Types;
+using Printborg.Interfaces;
+using Printborg.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintborgGH.Types {
+    public class GH_DeforumSettings :GH_Goo<DeforumSettings> {
+        #region PROPERTIES
+        public override bool IsValid => true;
+        public override string TypeName { get { return "Printborg.Types.DeforumSettings"; } }
+        public override string TypeDescription { get { return "Settings for DeforumJob"; } }
+
+        public override DeforumSettings Value { get => base.Value; set => base.Value = value; }
+        #endregion
+
+
+        public GH_DeforumSettings() {
+            this.Value = null;
+        }
+
+        public GH_DeforumSettings(DeforumSettings settings) {
+            this.Value = settings;
+        }
+
+        public GH_DeforumSettings(GH_DeforumSettings source) {
+            this.Value = source.Value;
+        }
+
+        public override IGH_Goo Duplicate() {
+            return new GH_DeforumSettings(this);
+        }
+
+        public override string ToString() {
+            if (this.Value != null) {
+                string output = "Printborg.Types.DeforumSettings";
+                return output;
+            }
+            return "No settings specified!";
+        }
+
+        public override bool CastTo<Q>(ref Q target) {
+            return false;
+        }
+
+        public override bool CastFrom(object source) {
+            return false;
+        }
+    }
+}
