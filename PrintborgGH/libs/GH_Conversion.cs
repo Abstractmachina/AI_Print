662c8e7a9b1c8fab4e6791cdb4675022984591d2
Tao <taole.chen@gmail.com>
2024-01-24 15:42:35 +0100

    generic conversion method to Dictionary<K,V> for GH_Dict added.

--- PrintborgGH/libs/GH_Conversion.cs
+++ PrintborgGH/libs/GH_Conversion.cs
@@ -1,4 +1,6 @@
-﻿using Grasshopper.Kernel.Types;
+﻿using GH_Dictionary;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,8 +8,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace PrintborgGH.libs {
-    internal static class GH_Conversion {
+    public static class GH_Convert {
         //if (GH_Convert.ToGHCurve(goo, GH_Conversion.Both, ref castCurve)) {
         //ToGHImage(IGH_Goo goo, )
+
+        public static Dictionary<K,V> ToDictionary<K,V>(GH_Dict input) {
+            //try to convert gh_dict to Dictionary<string, string>
+            var out_dict = new Dictionary<K,V>();
+            foreach (KeyValuePair<string, IGH_Goo> entry in input.Value) {
+                var key =  (K)Convert.ChangeType(entry.Key, typeof(K));
+                var val = (V)Convert.ChangeType(entry.Value.ScriptVariable(), typeof(V));
+
+                out_dict.Add(key, val);
+
+            }
+            return out_dict;
+        }
+
     }
 }
