2239389cd8e6338a0edaed3e7d891acaa3caaef2
Tao <taole.chen@gmail.com>
2024-02-01 23:08:43 +0100

    refactor

--- Printborg/interfaces/IJobReceipt.cs
+++ Printborg/interfaces/IJobReceipt.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Printborg.interfaces {
+namespace Printborg.Interfaces {
     
     /// <summary>
     /// Simple object that informs user whether their submission was successful or not.
